40f45e3b6eb787239bc467e46d05c3cf6a619adb
fockl <j4xn3gvbxqtfdnxg@gmail.com>
2024-11-17 14:40:44 +0900

    [update] pybind11

--- cmake/makelib.cmake
+++ cmake/makelib.cmake
@@ -16,6 +16,12 @@ add_library(${fmma_target} ${fmma_sources})
 set_common_properties(${fmma_target})
 set_blas(${fmma_target})
 
+if(FMMA_PYTHON)
+  add_subdirectory(extern/pybind11)
+  pybind11_add_module(pyfmma cpython/pyfmma.cpp)
+  target_link_libraries(pyfmma PRIVATE ${fmma_target})
+endif()
+
 set_target_properties(${fmma_target} PROPERTIES OUTPUT_NAME ${fmma_link})
 
 install(
