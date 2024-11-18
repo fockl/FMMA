33b3539235f30d832a14327b9b2dbabd19efd340
fockl <j4xn3gvbxqtfdnxg@gmail.com>
2024-11-19 03:54:37 +0900

    [update] for pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,30 @@ ext_modules = [
     Pybind11Extension("pyfmma", ["cpython/pyfmma.cpp"] + sorted(glob("src/*.cpp")))
     ]
 
+URL = 'https://github.com/fockl/FMMA'
+AUTHOR = 'fockl'
+AUTHOR_EMAIL = 'j4xn3gvbxqtfdnxg@gmail.com'
+
+with open('README.md', encoding='utf-8') as f:
+  readme = f.read()
+LONG_DESCRIPTION = readme
+
 setup(
     name="pyfmma",
+    version="0.1.1",
+    description="pyfmma: Fast Multipole Method for arbitrary functions",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    download_url=URL,
+    license='MIT',
     ext_modules=ext_modules,
-    install_requires=[])
+    classifiers=[
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: C++',
+      'Programming Language :: Python',
+      ],
+    install_requires=['setuptools<72.2.0', 'pybind11'],
+    )
